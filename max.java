b9b5f7ee165f22c5f2f5f0ecc2618dc38739cff2
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-08 11:04:08 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int max,i;
+	 Scanner sc=new Scanner(System.in);
+	 int a[]=new int[10];
+	 for(i=0;i<10;i++)
+	       {
+	          	a[i]=sc.nextInt();
+	       }
+	       max=a[0];
+	       for(i=0;i<10;i++)
+	       {
+	 	         if(max<a[i])
+	 	         {
+	 		        max=a[i];
+	 	         }
+	       }
+	 System.out.print(max);// your code goes here
+	}
+}
